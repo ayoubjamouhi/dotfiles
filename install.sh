b94db1adbd7d042b38557d356cb6aef8386196ef
ayoubjamouhi <gemouhi@gmail.com>
2022-12-31 17:52:38 -0500

    add install fedora

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
-picom
-polybar
-alactritty
-bspwm
-sx
-zshrx
+picom -> dnf install
+polybar -> dnf install
+alactritty -> install from .rpm
+bspwm -> dnf install
+sxhkd -> dnf onstall
+zshrc
 ohmyzsh
 zsh-autosuggestions
-ripgrep
+ripgrep -> dnf install
 nvim
 plug-install
 powerlevel10k
 libapache
 a2enmod rewrite
+rofi -> dnf install
+pulseaudio -> dnf install
+vlc
+nodejs
+composer
+php
+nginx
+operator mono
+vscode
+JetBreains MOno font
+pulseaudio
+brightnessctl
