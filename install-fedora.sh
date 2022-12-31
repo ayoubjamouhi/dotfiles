85c617d8d3cd1e743972517a8b4f1f3748ef018f
ayoubjamouhi <gemouhi@gmail.com>
2022-12-31 18:27:09 -0500

    Add new

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,13 +1,24 @@
-sudo dnf install picom polybar bspwm sxhkd alacritty rofi nodejs brightnessctl pulseaudio zsh curl stow --skip-broken 
+sudo dnf install picom polybar bspwm sxhkd alacritty rofi nodejs brightnessctl pulseaudio zsh curl stow ripgrep--skip-broken
 
+# oh my zsh
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "instaling oh my zsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi;
 
-
+# zsh autosuggestions
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
     echo "installing auto suggestion"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi;
 
+# vim plug
+if [ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]; then
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi;
+
+# powerlevel10k
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi;
